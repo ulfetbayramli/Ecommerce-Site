116f8c6794159ca3de91248e7fad59206317033a
ulfetbayramli <ulfetb034@gmail.com>
2023-03-03 20:40:04 +0400

    Done

--- SuperB/settings.py
+++ SuperB/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'ssjsj')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'sometext')
+'av$8q@=(q=qw=#f=8a4@tt56#a60bier5dy*uwdew$g8z(j9!m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if os.environ.get('DEBUG') else True
